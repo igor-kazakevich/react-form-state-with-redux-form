1d8bb4addbe187ef0aae277a1b14e85e3a54eaef
Igor Kazakevich <igor.kazakevich79@gmail.com>
2020-06-16 21:30:38 +0300

    react-form-state-with-redux-form

---
+++ src/validation/index.js
@@ -0,0 +1,29 @@
+export const required = value =>
+  value ? undefined : 'Value is required';
+
+export const minLength = value =>
+  value.length < 4
+    ? 'Value must be at least 4 characters'
+    : undefined;
+
+export const maxLength = value =>
+  value.length > 10
+    ? 'Value too long'
+    : undefined;
+
+export const matchesPassword = (value, allValues) =>
+  value === allValues.password ? undefined : 'Password must match';
+
+export const asyncValidate = async values => {
+  const sleep = ms => new Promise(resolve => setTimeout(
+    resolve,
+    ms
+  ));
+  await sleep(1000);
+
+  if (['kent', 'andy', 'john', 'joel'].includes(values.username)) {
+    return Promise.reject({
+      username: 'Username already taken'
+    });
+  }
+};
